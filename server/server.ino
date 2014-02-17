e65da43a78c066259341560dbb73504109524856
Tim Nordloh <tim@nordloh.com>
2014-02-17 15:06:21 -0700

    fixing minor coding issues

--- server/server.ino
+++ server/server.ino
@@ -2,7 +2,8 @@
 
 #include <Wire.h>
 #include "RTClib.h"
-#include <VirtualWire.h>  // you must download and install the VirtualWire.h to your hardware/libraries folder
+//virtualwire function, to support wireless connections between arduino units
+#include <VirtualWire.h>  
 
 RTC_DS1307 rtc;
 char date[12];
@@ -13,7 +14,7 @@ boolean DEBUG=false;
 void setDateTime() {
   Serial.println("set date here");
   Serial.readBytes(date,11);
-  date[11] = '\0';
+  date[11] = '\0'; //add \0 to end of string, otherwise, we read off the end of the array 
   Serial.println(date);
   boolean timeentered=false;
   while (!timeentered) {
@@ -26,7 +27,7 @@ void setDateTime() {
       if(command==84) {
         timeentered=true;
         Serial.readBytes(time,8);
-        time[8] = '\0';
+        time[8] = '\0'; //add \0 to end of string, otherwise, we read off the end of the array 
         Serial.println(time);
         rtc.adjust(DateTime(date, time));
       }
@@ -36,11 +37,11 @@ void setDateTime() {
 void setup () {
   Serial.begin(9600);
 
-    vw_set_ptt_inverted(true); // Required for RF Link module
-    vw_setup(2000);                 // Bits per sec
-    vw_set_tx_pin(9);                // pin 9 is used as the transmit data out into the TX Link module, change this to suit your needs.
-    vw_set_rx_pin(11);           // We will be receiving on pin 23 (Mega) ie the RX pin from the module connects to this pin.
-    vw_rx_start();
+  vw_set_ptt_inverted(true); // Required for RF Link module
+  vw_setup(2000);                 // Bits per sec
+  vw_set_tx_pin(9);                // pin 9 is used as the transmit data out into the TX Link module, change this to suit your needs.
+  vw_set_rx_pin(11);           // We will be receiving on pin 23 (Mega) ie the RX pin from the module connects to this pin.
+  vw_rx_start();
 #ifdef AVR
   Wire.begin();
 #else
@@ -53,57 +54,35 @@ void setup () {
     // following line sets the RTC to the date & time this sketch was compiled
     //rtc.adjust(DateTime(__DATE__, __TIME__));
   }
-
-    
 }
-
-
 
 void loop () {
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
   Serial.println("receiving");
- 
+
   if (vw_get_message(buf, &buflen)) // check to see if anything has been received
   {
-    int i;
+    //int i;
     // Message with a good checksum received.
-    char temp=0;;    
-    for (i = 0; i < buflen; i++) {   
+    char temp=0;  
+    for (int i = 0; i < buflen; i++) {   
       temp=(char)buf[i];
       Serial.print(temp);  // the received data is stored in buffer
     }
     Serial.print(" ");
     Serial.print(counter++);
-    Serial.println("");
+    Serial.println();
   }
   if (Serial.available()) {      // Look for char in serial que and process if found
     int command = Serial.read();
     Serial.println(command);
     if(command==68) {
-    setDateTime();
-//      boolean timeentered=false;
-//      while (!timeentered) {
-//        delay(3000);   
-//        Serial.print(date);
-//        Serial.println("currently stuck waiting for time");
-//        if(Serial.available()) {     
-//          command = Serial.read();
-//          Serial.println(command);
-//          if(command==84) {
-//            timeentered=true;
-//            Serial.readBytes(time,8);
-//            time[8] = '\0';
-//            Serial.println(time);
-//            rtc.adjust(DateTime(date, time));
-//          }
-//        }
-//      }
+      setDateTime();
     }
-
   }
   DateTime now = rtc.now();
-  //Serial.println(__DATE__);
+  //Serial.println(__DATE__);  //uncomment if I forget the format for entering in time
   //Serial.println(__TIME__);
   Serial.print(now.year(), DEC);
   Serial.print('/');
@@ -117,16 +96,16 @@ void loop () {
   Serial.print(':');
   Serial.print(now.second(), DEC);
   Serial.println();
-  
+
   //Serial.print(" since midnight 1/1/1970 = ");
-  //Serial.print(now.unixtime());
+  Serial.println(now.unixtime());
   //Serial.print("s = ");
   //Serial.print(now.unixtime() / 86400L);
   //Serial.println("d");
-  if(DEBUG) {
+  //if(DEBUG) {
     // calculate a date which is 7 days and 30 seconds into the future
     //DateTime future (now.unixtime() + 7 * 86400L + 30);
-    
+
     //Serial.print(" now + 7d + 30s: ");
     //Serial.print(future.year(), DEC);
     //Serial.print('/');
@@ -142,8 +121,6 @@ void loop () {
     //Serial.println();
     //    
     //Serial.println();
-  }
+  //}
   delay(3000);
 }
-
-
--- snakemail/snakemail.ino
+++ snakemail/snakemail.ino
@@ -4,76 +4,75 @@
 // rx pin 23 on mega
 
 #include <VirtualWire.h>  // you must download and install the VirtualWire.h to your hardware/libraries folder
-#undef int
-#undef abs
-#undef double
-#undef float
-#undef round
-   int counter = 0;
-   int watersensor = 2;
-   int led = 4;
-   int watersensorpower=3;
-   int watersensorstate=0;
-   int watersensorcounter=0;
+int counter = 0;
+int watersensor = 2;
+int led = 4;
+int watersensorpower=3;
+int watersensorstate=0;
+int watersensorcounter=0;
 void setup()
 {
-    Serial.begin(9600);    
+  Serial.begin(9600);    
 
-// Initialise the IO and ISR
-    vw_set_ptt_inverted(true);    // Required for RX Link Module
-    vw_setup(2000);                   // Bits per sec
-    vw_set_rx_pin(11);           // We will be receiving on pin 11.
-    vw_set_tx_pin(9);                // pin 9 is used as the transmit data out into the TX Link module, change this to suit your needs.
-    vw_rx_start();                      // Start the receiver
-    pinMode(watersensorpower,OUTPUT);
-    pinMode(watersensor,INPUT);
-    pinMode(led,OUTPUT);
+  // Initialise the IO and ISR
+  vw_set_ptt_inverted(true);    // Required for RX Link Module
+  vw_setup(2000);                   // Bits per sec
+  vw_set_rx_pin(11);           // We will be receiving on pin 11.
+  vw_set_tx_pin(9);                // pin 9 is used as the transmit data out into the TX Link module, change this to suit your needs.
+  vw_rx_start();                      // Start the receiver
+  pinMode(watersensorpower,OUTPUT);
+  pinMode(watersensor,INPUT);
+  pinMode(led,OUTPUT);
 }
 
 void loop()
 {
-    uint8_t buf[VW_MAX_MESSAGE_LEN];
-    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
 
-    watersensorcounter++;
-    if(watersensorcounter % 6 == 0) {
+  watersensorcounter++;
+  if(watersensorcounter % 6 == 0) {
 
-      digitalWrite(watersensorpower,HIGH);
-      int watersensorstate = digitalRead(watersensor);
-      Serial.print(watersensorstate);
-      delay(10);
-      if(watersensorstate == HIGH) {
-        digitalWrite(led,LOW);
-        Serial.println("we have water");
-        delay(1000);
-      } else {
-        Serial.println("we don't have water");
-        const char *msg = "Snakes have no water";       // this is your message to send
-        Serial.println("sending");
-        digitalWrite(led,HIGH);
-        for(int i = 0;i<10;i++) {
-          vw_send((uint8_t *)msg, strlen(msg));
-          vw_wait_tx();          // Wait for message to finish
-          Serial.println("sending");
-        }
-        delay(1000);
-      }
-      digitalWrite(watersensorpower,LOW);
-    } else {
+    digitalWrite(watersensorpower,HIGH);
+    int watersensorstate = digitalRead(watersensor);
+    Serial.print(watersensorstate);
+    delay(10);
+    if(watersensorstate == HIGH) {
+      digitalWrite(led,LOW);
+      Serial.println("we have water");
       delay(1000);
-    }   
-    if (vw_get_message(buf, &buflen)) // check to see if anything has been received
-    {
+    } 
+    else {
+      Serial.println("no water");
+      const char *msg = "SNAKE:WATERLOW";       // this is your message to send
+      digitalWrite(led,HIGH);
+      for(int i = 0;i<10;i++) {
+        vw_send((uint8_t *)msg, strlen(msg));
+        vw_wait_tx();          // Wait for message to finish
+        Serial.println("sending");
+      }
+      delay(1000);
+    }
+    digitalWrite(watersensorpower,LOW);
+  } 
+  else {
+    delay(1000);
+  }   
+  if (vw_get_message(buf, &buflen)) // check to see if anything has been received
+  {
     int i;
-     // Message with a good checksum received.
-    char temp=0;;    
+    // Message with a good checksum received.
+    char temp=0;
+    ;    
     for (i = 0; i < buflen; i++)
-    {   temp=(char)buf[i];
-        Serial.print(temp);  // the received data is stored in buffer
-        }
-        Serial.print(" ");
-        Serial.print(counter++);
-    Serial.println("");
-     }
-     //Serial.println("working");
+    {   
+      temp=(char)buf[i];
+      Serial.print(temp);  // the received data is stored in buffer
+    }
+    Serial.print(" ");
+    Serial.print(counter++);
+    Serial.println();
+  }
+  //Serial.println("working");
 }
+
